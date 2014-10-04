e403fb5225be8d1611bfaa30f3abf44b7fb9db51
Jeremiah Marks <jeremiah@jlmarks.org>
2014-10-04 15:23:54 -0700

    making some more progress.  Have the strings where they parse, for the most part.  I am still looking for cases that break the parsing scheme.  Working on uploading to database now.

--- python/testgrounds.py
+++ python/testgrounds.py
@@ -1,6 +1,7 @@
 import config
 import MySQLdb
-
+import examples
+import re
 
 def red():
 	db=MySQLdb.connect(host=config.MySQLConnection.hostname,user=config.MySQLConnection.username, passwd=config.MySQLConnection.password, db=config.MySQLConnection.dbname)
@@ -12,6 +13,7 @@ def red():
 	for x in range(0,numrows):
 	    row = cursor.fetchone()
 	    print row[0], "-->", row[1] , " " , row[2], " - ", row[3]
-
+	header,body = examples.text1.split
+	re.compile
 if __name__ == '__main__':
 	red()
